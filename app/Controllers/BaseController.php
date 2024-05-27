bcde18b3400e2601c54e6d600f3fef4b74144cf6
laurarzur <laura.arzur@gmail.com>
2024-05-27 14:48:49 +0200

    twig installed

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -35,7 +35,8 @@ abstract class BaseController extends Controller
      *
      * @var list<string>
      */
-    protected $helpers = [];
+    protected $helpers = []; 
+    protected $twig;
 
     /**
      * Be sure to declare properties for any property fetch you initialized.
@@ -52,7 +53,7 @@ abstract class BaseController extends Controller
         parent::initController($request, $response, $logger);
 
         // Preload any models, libraries, etc, here.
-
+        $this->twig = new \Kenjis\CI4Twig\Twig();
         // E.g.: $this->session = \Config\Services::session();
     }
 }
